6842f1e4359b6d22d07f797467dbf48dd2638257
nourKhattab-nour <nour.khattab90@gmail.com>
2025-08-31 17:10:47 +0300

    Update SearchPlayer.jsx

--- movie-db-app/src/pages/SearchPlayer.jsx
+++ movie-db-app/src/pages/SearchPlayer.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Header from "../components/HomePage/Header.jsx"
 import Footer from "../components/Footer/Footer.jsx";
-import Main from "../components/SearchPage/MainSection,jsx";
+import Main from "../components/SearchPage/MainSection.jsx";
 
 const SearchPlayer = () => {
   return (
